0d132534c157cf2aadf0ae7a57117836e7f78508
romabezduploma <roman.balkan@gmail.com>
2019-12-13 14:59:42 +0000

    coo

--- js/index.js
+++ js/index.js
@@ -1,51 +1,73 @@
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
-const listItem = document.createElement('li');
-
-function btnPriorityClicked() {
-    listItem.classList.add('red-color-priority');
-}
-
-function btnCrossOutClicked() {
-    listItem.classList.add('cross-out');
-}
-
-function btnDeleteClicked() {
-    $('.btn-delete').click(() => $('li').toggle(3000));
-}
 
 button.onclick = function () {
     let myPlan = input.value;
-    
-    if(myPlan === '') {
-        alert('Incorrect input, please enter yput plan again');
-    }
     input.value = '';
 
+    const listItem = document.createElement('li');
     const listText = document.createElement('span');
     const listCrossOut = document.createElement('button');
-    const listPriority = document.createElement('button');
+    const listPriorityOne = document.createElement('button');
+    const listPrioritySecond = document.createElement('button');
     const listBtnDelete = document.createElement('button');
-    
+
 
     listItem.appendChild(listText);
     listText.textContent = myPlan;
+    listItem.appendChild(listPriorityOne);
+    listPriorityOne.textContent = 'Priority Main';
+    listItem.appendChild(listPrioritySecond);
+    listPrioritySecond.textContent = 'Priority Second';
     listItem.appendChild(listCrossOut);
     listCrossOut.textContent = 'Completed';
-    listItem.appendChild(listPriority);
-    listPriority.textContent = 'Priority';
     listItem.appendChild(listBtnDelete);
     listBtnDelete.textContent = 'Delete';
     listBtnDelete.classList.add('btn-delete');
 
     list.appendChild(listItem);
 
+    function btnPriorityOneClicked() {
+        listItem.classList.add('priority-one');
+    }
+
+    function btnPrioritySecondClicked() {
+        listItem.classList.add('priority-two');
+    }
+
+    function btnCrossOutClicked() {
+        listItem.classList.add('cross-out');
+    }
+
+    function btnDeleteClicked() {
+        list.removeChild(listItem);
+    }
 
     listCrossOut.addEventListener('click', btnCrossOutClicked);
-    listPriority.addEventListener('click', btnPriorityClicked);
+    listPriorityOne.addEventListener('click', btnPriorityOneClicked);
+    listPrioritySecond.addEventListener('click', btnPrioritySecondClicked);
     listBtnDelete.addEventListener('click', btnDeleteClicked);
-    
 
     input.focus();
 }
+
+const sect = document.querySelector('section');
+const clock = document.querySelector('.clock');
+const reset = document.querySelector('.reset-all');
+
+function btnShowClock() {
+    const updateTime = () => clock.innerHTML = new Date().toLocaleTimeString();
+    setInterval(updateTime, 1000);
+    showClock.textContent = "Hide clock";
+}
+
+const showClock = document.querySelector('.show-clock');
+showClock.addEventListener('click', btnShowClock);
+
+function resetList() {
+    $('.reset-all').click(() => $('li').toggle(2000));
+}
+
+reset.addEventListener('click', resetList);
+
